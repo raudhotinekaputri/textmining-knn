66004424c3e879c4a1eaee717f16211112cca941
rizkythahatmaputri <rithakendal@gmail.com>
2023-09-26 10:01:42 +0700

    styling lagi training

--- training.php
+++ training.php
@@ -67,10 +67,7 @@
             <a href="index.html" class="btn btn-secondary">Kembali ke Halaman Awal</a>
         </div>
     </div>
-    <footer class="text-center py-3 text-white">
-        &copy; 2023 Document Classification
-    </footer>
-    
+
     <!-- Menambahkan tautan ke Bootstrap JavaScript dan jQuery (diperlukan oleh Bootstrap) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
